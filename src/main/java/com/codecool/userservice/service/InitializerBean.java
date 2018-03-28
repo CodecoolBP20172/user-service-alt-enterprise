d9a2c80ca9ff99852ca778906b27d1df42d8d03f
Soma Szolyák <soma.szolyak@gmail.com>
2018-03-28 16:05:20 +0200

    added another 2 users nnow ther are 3 of them

--- src/main/java/com/codecool/userservice/service/InitializerBean.java
+++ src/main/java/com/codecool/userservice/service/InitializerBean.java
@@ -10,6 +10,10 @@ public class InitializerBean {
 
     public InitializerBean(UserService userService) throws ParseException {
         User user1 = new User("Dezső", "Kosztolányi", "dktheprime", "pa55w0rd", "dkdezs@nyugat.hu", "Budapest", 18);
+        User user2 = new User("Babra", "Megy", "username", "psw", "email@email.hu", "City", 10000);
+        User user3 = new User("Málészájú", "Kecskefia", "babababa", "qwe", "asd@asd.hu", "Város", 200000000);
         userService.registerUser(user1);
+        userService.registerUser(user2);
+        userService.registerUser(user3);
     }
 }
